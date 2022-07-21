7771399e6a9fd340564ef2bd1d3dcb4e52234ebf
0X0ŽĒR∅⁰ <71743035+MRZER00@users.noreply.github.com>
2022-07-21 11:27:04 +0300

    Update index.ts

--- Projects-Web-Advanced/Resize-Images/src/index.ts
+++ Projects-Web-Advanced/Resize-Images/src/index.ts
@@ -1,27 +1,27 @@
 import express from 'express';
-import route from "./routers/index";
+import route from './routers/index';
 const app = express();
 const router = express.Router();
 const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
-app.use('/api', route)
-app.get('/', (_req, res)=>{
+app.use('/api', route);
+app.get('/', (_req: express.Request, res: express.Response): void => {
   res.render('index');
 });
 
 
 // page not found
-app.use( (_req, res, next) => {
-  res.status(404)
+app.use((_req: express.Request, res: express.Response) => {
+  res.status(404);
   fs.readFile('../Project-Web-Advanced/views/images/404.jpg',
-    (_err:any, img: any) => res.end(img) );
+    (_err: Error, img: string ) => res.end(img));
 });
 
 // server run in localhost:port
-app.listen(2020,()=>{
-  console.log(`server started at localhost:2020}`);
+app.listen(2020, () => {
+  console.log('server started at localhost:2020}');
 });
 
 export default app;
